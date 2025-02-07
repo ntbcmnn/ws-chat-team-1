2c96c7f77e9ecddaedf0f0edf5f53176921d5732
Kris <kristinaovchinnikova22@gmail.com>
2025-02-07 17:05:20 +0600

    [API] WSC-4 - done function run

--- backend/index.ts
+++ backend/index.ts
@@ -3,11 +3,16 @@ import expressWs from 'express-ws';
 import cors from 'cors';
 import {WebSocket} from 'ws';
 import usersRouter from './routers/users';
+import config from "./config";
+import mongoose from "mongoose";
 
 const app = express();
 expressWs(app);
 const port = 8000;
 app.use(cors());
+
+app.use(express.json());
+app.use(express.static('public'));
 
 const router = express.Router();
 app.use(router);
@@ -26,6 +31,17 @@ router.ws('/chat', (ws, req) => {
         console.log('Client total: ' + connectedClients.length);
     });
 });
+
+const run: () => Promise<void> = async () => {
+    await mongoose.connect(config.db);
+
+    app.listen(port, () => {
+        console.log(`Listening on port http://localhost:${port}`);
+    });
+};
+
+run().catch((err) => console.log(err));
+
 
 app.listen(port, () => {
     console.log(`Server started on ${port} port`);
