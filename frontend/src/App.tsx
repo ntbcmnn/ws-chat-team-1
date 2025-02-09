c39813fe8c2b75e84037599fcf266b1593fb8e53
Natalya Botsman <botsmannatashaa@gmail.com>
2025-02-09 15:42:22 +0600

    [FRONT] WSC-6 Added a route for non-existing pages and imported toolbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
-import "./App.css";
+import './App.css';
+import Toolbar from './components/UI/Toolbar/Toolbar.tsx';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  return <></>;
+  return <>
+    <Toolbar/>
+    <div className="container my-5">
+      <Routes>
+        <Route path="*" element={<h2 className="text-center">Page not found</h2>}/>
+      </Routes>
+    </div>
+  </>;
 };
 
 export default App;
