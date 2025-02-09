a284d942accf44d39098ed8c461241a7caac665c
Natalya Botsman <botsmannatashaa@gmail.com>
2025-02-09 16:19:54 +0600

    [FRONT] WSC-9 Created routes for login and register pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
 import Toolbar from './components/UI/Toolbar/Toolbar.tsx';
 import { Route, Routes } from 'react-router-dom';
+import LoginPage from './containers/LoginPage/LoginPage.tsx';
+import RegisterPage from './containers/RegisterPage/RegisterPage.tsx';
 
 const App = () => {
   return <>
     <Toolbar/>
     <div className="container my-5">
       <Routes>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/register" element={<RegisterPage/>}/>
         <Route path="*" element={<h2 className="text-center">Page not found</h2>}/>
       </Routes>
     </div>
